2bc3d260d28ed9e1d97d1c114c7e4c3dc43e1d80
vsaprykina <92681607+vsaprykina@users.noreply.github.com>
2024-05-13 01:40:10 +0300

    q

--- site1/main/models.py
+++ site1/main/models.py
@@ -14,17 +14,5 @@ class Appointment(models.Model):
     def __str__(self):
         return self.fullName
 
-    def send_sms(self):
-        # Здесь вы можете добавить вашу логику для отправки SMS
-        # Например, использовать Twilio для отправки SMS
-        # Пример отправки SMS с использованием Twilio:
-        account_sid = 'AC3d57c8c7d66679836b032d15da52172e'
-        auth_token = '1d9028c1a49f5398ec400056c065ce28'
-        client = Client(account_sid, auth_token)
 
-        message = client.messages.create(
-            body=f"Ваша запись на {self.appointmentDate} в {self.appointmentTime} на услугу {self.service} одобрена.",
-            from_='YOUR_TWILIO_NUMBER',
-            to=self.phoneNumber
-        )
 
